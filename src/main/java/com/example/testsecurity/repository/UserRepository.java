f91f5c639905de87140fec8131f34a551bb80108
dckim15 <dckim15@naver.com>
2024-03-17 11:33:27 +0900

    [수정] 회원 중복 검증/방지

--- src/main/java/com/example/testsecurity/repository/UserRepository.java
+++ src/main/java/com/example/testsecurity/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package com.example.testsecurity.repository;
 
+import com.example.testsecurity.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.example.testsecurity.entity.UserEntity;
 
 /**
  * JpaRepository<Entity클래스, Entity의 @Id변수의 type>
@@ -11,9 +10,9 @@ import com.example.testsecurity.entity.UserEntity;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer>{				
 
-//	boolean existsByUsername(String username);
+	boolean existsByUsername(String username);                      // JPA 커스텀 메소드.(existsBy*). (참고: @Query 를 이용해서 쿼리를 사용자 정의로 지정해줄수도 있는듯.)
 
-//	UserEntity findByUsername(String username);						// JPA 커스텀 메소드. (findBy)
+//	UserEntity findByUsername(String username);						// JPA 커스텀 메소드. (findBy*)
 	
 	
 
--- src/main/java/com/example/testsecurity/service/JoinService.java
+++ src/main/java/com/example/testsecurity/service/JoinService.java
@@ -1,12 +1,11 @@
 package com.example.testsecurity.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
 import com.example.testsecurity.dto.JoinDTO;
 import com.example.testsecurity.entity.UserEntity;
 import com.example.testsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
 public class JoinService {
@@ -26,12 +25,14 @@ public class JoinService {
 	public void joinProcess(JoinDTO joinDTO) {
 
 		/**
-		 * 회원가입 처리전, 유저 중복 체크. (중복 검증)
+		 * 회원가입 처리전, 유저 중복 체크. (중복 방지 로직)
+		 *
+		 * (참고: 일반적으론, 프론트 단에도 아이디 중복 확인 로직을 추가해줘야 한다. 여기선 생략.)
 		 */
-//		boolean isUser = userRepository.existsByUsername(joinDTO.getUsername());
-//		if(isUser) {
-//			return;
-//		}
+		boolean isUser = userRepository.existsByUsername(joinDTO.getUsername());
+		if(isUser) {
+			return;
+		}
 		
 		
 		/**
